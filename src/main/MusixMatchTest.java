46be753ecc9958df110af74186003e5dfbc284f5
Sebastian Ånerud <anerud@student.chalmers.se>
2014-05-02 16:15:08 +0200

    Added wrapper jar for the musixmatch lyrics API

---
+++ src/main/MusixMatchTest.java
@@ -0,0 +1,14 @@
+package main;
+
+import org.jmusixmatch.MusixMatch;
+
+public class MusixMatchTest {
+
+	public static void main(String[] args){
+		
+		String apiKey = "Your MusixMatch API Key";
+		MusixMatch musixMatch = new MusixMatch(apiKey);
+		
+	}
+	
+}
